6eb4d6b8d4eb4e9fc7d4a147589347c0c850c4de
rodrigo <rodrigoswp@gmail.com>
2018-11-05 21:46:00 -0200

    novo index

--- vet/index.php
+++ vet/index.php
@@ -1,15 +1,28 @@
-<?php
+<?php  
+define("_APP", dirname(__FILE__) . 'vetapp');
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
-require 'vendor/autoload.php';
+//ob_end_clean();
 
-$app = new \Slim\App();
+# === slim
+# ==================================================
+require '../vendor/autoload.php';
+$app = new \Slim\App([
+    'settings' => [
+        'determineRouteBeforeAppMiddleware' => true,
+        'displayErrorDetails' => true,
+        'addContentLengthHeader' => false
+    ]
+]);
 
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    echo "Hello, $name!";
-});
+require_once  '../src/config/config.php';
+require_once  '../src/models/appModels.php';  
+require_once  '../src/helpers/appHelpers.php';
+require_once  '../src/controllers/appControllers.php';
 
+//require_once  '../src/handlers/exception.php';
+
+
+
+# === run slim
 $app->run();
